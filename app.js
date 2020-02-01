49621097938700e392d64bc25d61829412fcc07e
Dinika <dinikasaxenas@gmail.com>
2020-02-01 15:34:19 +0000

    Create an express server
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+	res.send('Helllllooo boys');
+});
+
+app.listen(port, () => {
+	console.log('App listening on port ', port);
+})
