729c73ceb60adb9475044ae084f3899b36240845
Dinika <dinikasaxenas@gmail.com>
2020-02-02 00:55:15 +0000

    Add a route that sets a custom header on response
    Signed-off-by: Dinika <dinikasaxenas@gmail.com>

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ app.get('/', (req, res) => {
 	res.send('Helllllooo boys');
 });
 
+app.get('/demo', (req, res) => {
+	res.set({ 'X-Rated': 'Buttons' });
+	res.status(418);
+	res.send('And girls too')
+});
+
 app.listen(port, () => {
 	console.log('App listening on port ', port);
-})
+});
